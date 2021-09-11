9aac362295e6a162d38ed485e419133793a7e4e3
그램 <darkzhon@hanmail.net>
2021-09-12 03:51:06 +0900

    스토리 북 config에 stories 폴더 자동 등록 하기 및 테스트

--- src/stories/NewCounterStory.jsx
+++ src/stories/NewCounterStory.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import Input from '../03/18.Input';
 import NewCounter from '../03/12.newCounter';
-
-storiesOf('Input', module)
-  .add('기본 설정', () => <Input name="name" />)
-  .add('라벨 예제', () => <Input name="name" label="이름" />);
 
 storiesOf('newCounter', module).add('기본설정', () => <NewCounter count={0} />);
