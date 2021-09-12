296a09ee5ba26c8f373b88a6b807d26a0ddf23b8
그램 <darkzhon@hanmail.net>
2021-09-12 20:10:01 +0900

    addon-jsx 설치하기

--- src/stories/inputStory.jsx
+++ src/stories/inputStory.jsx
@@ -5,8 +5,6 @@ import { action } from '@storybook/addon-actions';
 import Input from '../03/18.Input';
 
 storiesOf('Input', module)
-  .add('기본 설정', () => <Input name="name" />)
-  .add('라벨 예제', () => <Input name="name" label="이름" />)
-  .add('onChange 예제', () => <Input name="name" onChange={action("onChange 이벤트 발생")} />);
-
-storiesOf('newCounter', module).add('기본설정', () => <NewCounter count={0} />);
+  .addWithJSX('기본 설정', () => <Input name="name" />)
+  .addWithJSX('라벨 예제', () => <Input name="name" label="이름" />)
+  .addWithJSX('onChange 예제', () => <Input name="name" onChange={action("onChange 이벤트 발생")} />);
