9007aaaaafc3178af6423f7760c5e3dca30ad0e4
그램 <darkzhon@hanmail.net>
2021-09-16 00:22:36 +0900

    enzyme 라이브러리 설치후 쉽게 테스트 하기

--- src/__tests__/04/input.test.jsx
+++ src/__tests__/04/input.test.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import Input from '../../03/18.Input'
 
 describe('<Input>', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Input name="name" />, div);
-    ReactDOM.unmountComponentAtNode(div);
-    expect(React.isValidElement(<Input name="test_name" />)).toBeTruthy();
+  it('크래시 없이 제대로 렌더링 되는지?', () => {      //해당 컴포넌트가, 크래쉬 없이 제대로 렌더링이 되었는지 확인
+    expect( () => {
+      shallow(<Input name="test_name" />);       // 현재 컴포넌트의 생명주기만 테스트 하는 함수
+    }).not.toThrow();           // 컴포넌트의 출력 오류를 출력해줌
   });
+
+  it('한개의 element 를 가지는가?', () => {
+    const wrapper = shallow(<Input name="test_name" />)
+    expect(wrapper.length).toEqual(1);
+    expect(wrapper).toHaveLength(1);
+  });
+
 });
