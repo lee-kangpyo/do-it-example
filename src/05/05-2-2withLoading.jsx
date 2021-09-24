cd425eec39f97afe5d31c2f0a2efd1eb73783d6e
그램 <darkzhon@hanmail.net>
2021-09-24 23:19:24 +0900

    다중 커링으로 하이오더컴포넌트 규칙(컴포넌트를 만들때 인자 하나를 전달한다) 지키기

--- src/05/05-2-2withLoading.jsx
+++ src/05/05-2-2withLoading.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
-export default function withLoading(WrappedComponent, loadingMessage = "로딩 중") {
-  const { displayName, name: componentName } = WrappedComponent;
-  const WrappedComponentName = displayName || componentName;
+export default function withLoading(loadingMessage = "로딩 중") {
+  function withLoading(WrappedComponent){
+    const { displayName, name: componentName } = WrappedComponent;
+    const WrappedComponentName = displayName || componentName;
 
-  function WithLoading({isLoading, ...otherProps}){
-    if(isLoading) return loadingMessage;
+    function WithLoading({isLoading, ...otherProps}){
+      if(isLoading) return loadingMessage;
 
-    return (
-      <WrappedComponent {...otherProps} />
-    );
+      return (
+        <WrappedComponent {...otherProps} />
+      );
+    }
+    WithLoading.displayName = `withLoading(${WrappedComponentName})`;
+    return WithLoading;
   }
-  WithLoading.displayName = `withLoading(${WrappedComponentName})`;
-  return WithLoading;
 }
