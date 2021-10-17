87ac2308f78a4aa98fe1830ee20f6bc15d6c17f8
그램 <darkzhon@hanmail.net>
2021-10-17 22:53:12 +0900

    lifecyle에 compose 함수 적용하기

--- src/stories/LifecycleStory.jsx
+++ src/stories/LifecycleStory.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import { PageWithLoadData } from '../05/05-3-3.lifecycle';
+import { PageWithLoadData, PageWithLoadDataAndLoading } from '../05/05-3-3.lifecycle';
 
 storiesOf('lifecycle', module)
   .addWithJSX('loadData 예제', () => (
     <PageWithLoadData loadData={() => fetch('/').then(()=> 'hello')} />
-  ));
+  ))
+  .addWithJSX('로딩 메시지 예제', () => (
+    <PageWithLoadDataAndLoading loadData={() => fetch('/').then(() => 'hello')} />
+  ))
